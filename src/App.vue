299af600e3f59af73147b332cb7a584d2133ee63
kirsanovmike <kirsanovmike@gmail.com>
2024-03-28 00:38:14 +0300

    simple app

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <main>
+    <h1>
+      People invited to my party
+    </h1>
+    <input type="text" placeholder="Name" v-model="name" @keypress.enter="addInvitee">
+    <select v-model="gender" @keypress.enter="addInvitee">
+      <option :value="GENDER.MALE">Male</option>
+      <option :value="GENDER.FEMALE">Female</option>
+    </select>
+    <card v-for="(inv, index) in invitees" :key="index" :invitee="inv" />
+    <p>
+      maleCount: {{count.male}}
+    </p>
+    <p>
+      femaleCount: {{count.female}}
+    </p>
+  </main>
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import Card from "@/components/Card.vue";
+
+enum GENDER {
+  MALE,
+  FEMALE
+}
+
+interface Invitee {
+  id: number,
+  name: string,
+  gender: GENDER
+}
+
+const name = ref<string>("");
+const gender = ref<GENDER>(GENDER.MALE)
+const invitees = ref<Invitee[]>([])
+
+const addInvitee = ():void => {
+  if (name.value) {
+    invitees.value.push({
+      id: Math.floor(Math.random() * 1_000_000),
+      name: name.value,
+      gender: gender.value
+    })
+    name.value = "";
+    gender.value = GENDER.MALE;
+  }
+}
+
+interface Count {
+  male: number,
+  female: number
+}
+
+const count = computed<Count>(() => {
+  return invitees.value.reduce((acc, cur) => {
+    if (cur.gender === GENDER.MALE) {
+      return {
+        ...acc,
+        male: acc.male + 1
+      }
+    } else {
+      return {
+        ...acc,
+        female: acc.female + 1
+      }
+    }
+  }, {male: 0, female: 0})
+})
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ invitee.name }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+enum GENDER {
+  MALE,
+  FEMALE
+}
+
+defineProps<{
+  invitee: {
+    id: number,
+    name: string,
+    gender: GENDER
+  }
+}>()
+</script>
