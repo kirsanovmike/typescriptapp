5a2aefea188fd8ea0b2d2b40d1848f09d58b2568
kirsanovmike <kirsanovmike@gmail.com>
2024-03-28 18:04:38 +0300

    new stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+// pwa
+import {VitePWA} from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    VitePWA()
   ],
   resolve: {
     alias: {
